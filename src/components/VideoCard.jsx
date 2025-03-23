70e6971baf882cd95a22b677ea34815a0092bdc9
Dattatray Panchal <dattapanchal464@gmail.com>
2025-03-23 08:41:35 -0700

    some design changes in video card

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -3,11 +3,12 @@ import React, { useEffect, useState } from 'react'
 import { API_KEY, BASE_URL } from '../constants/YoutubeAPI';
 import Avatar from 'react-avatar';
 import moment from 'moment';
+import { useSelector } from 'react-redux';
 const VideoCard = ({ video }) => {
   const [ytIcon, setYtIcon] = useState();
   const [views, setViews] = useState();
   const { snippet } = video
-
+  const {isDarkMode} = useSelector((store)=>store.app);
   const formatViews = (num) => {
     if (num >= Math.pow(10, 9)) return (num / Math.pow(10, 9)).toFixed(1) + "B";
     if (num >= Math.pow(10, 6)) return (num / Math.pow(10, 6)).toFixed(1) + "M";
@@ -51,7 +52,7 @@ const VideoCard = ({ video }) => {
   }, [])
 
   return (
-    <div className='w-[100%] cursor-pointer hover:bg-pink-50 duration-300 rounded-lg'>
+    <div className='w-[100%] cursor-pointer duration-300 rounded-lg'>
       <div className="img-container w-full">
         <img className='rounded-lg w-full object-cover aspect-video' src={snippet?.thumbnails?.medium?.url} alt="" />
       </div>
@@ -61,11 +62,11 @@ const VideoCard = ({ video }) => {
         </div>
         <div className="channel-data">
           <h3 className='text-[1rem] font-bold'>{snippet?.title.slice(0, 30)}...</h3>
-          <h4 className='text-[0.9rem] text-zinc-800 font-[400]'>{snippet?.channelTitle}</h4>
+          <h4 className={`text-[0.9rem] ${isDarkMode ? "":"text-zinc-800 "} font-[400]`}>{snippet?.channelTitle}</h4>
           <div className='flex items-center gap-1'>
-            <h4 className='text-[0.8rem] text-zinc-700 font-[400]'>{formatViews(views)} views</h4>
-            <div className='w-[0.2rem] h-[0.2rem] bg-zinc-500 rounded-lg'></div>
-            <h4 className='text-[0.8rem] text-zinc-700 font-[400]'>{moment(snippet?.publishedAt).fromNow()}</h4>
+            <h4 className={`text-[0.8rem] ${isDarkMode ? "":"text-zinc-700"} font-[400]`}>{formatViews(views)} views</h4>
+            <div className={`w-[0.2rem] h-[0.2rem] ${isDarkMode ? "bg-zinc-100":"bg-zinc-500"} rounded-lg`}></div>
+            <h4 className={`text-[0.8rem]${isDarkMode ? "":" text-zinc-700"} font-[400]`}>{moment(snippet?.publishedAt).fromNow()}</h4>
           </div>
         </div>
       </div>
