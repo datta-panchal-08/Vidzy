0621d087f5bd8557ec4581db762845e7786e589f
Dattatray Panchal <dattapanchal464@gmail.com>
2025-03-26 18:13:20 +0530

    fixed the mistake where i used useDispatch instead of useSelector

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ButtonList from './ButtonList'
 import VideoContainer from './VideoContainer'
-import { useDispatch } from 'react-redux';
+import {useSelector } from 'react-redux';
 
 const Feed = () => {
-    const { isSidebarVisible } = useDispatch(store => store.app);
+    const { isSidebarVisible } = useSelector(store => store.app);
 
     return (
         <div className={` px-1 ${isSidebarVisible === true ? "md:w-[83%]  overflow-x-hidden " : "w-full overflow-x-auto "}`}>
